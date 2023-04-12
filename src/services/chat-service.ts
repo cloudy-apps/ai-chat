19c7dea4d93c1e1533601418d76b663ce5f772f8
Darlan Alves <me@darlanalv.es>
2023-04-12 16:16:39 +0200

    fix: send correct payload

--- src/services/chat-service.ts
+++ src/services/chat-service.ts
@@ -6,7 +6,12 @@ export interface Message {
 export class ChatService {
   async ask(question: Message, history: Message[] = []) {
     const messages = history.concat(question);
-    const options = { method: 'post', mode: 'cors', body: JSON.stringify({ messages }) };
+    const options = { 
+      method: 'post',
+      mode: 'cors',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({ messages }),
+    };
 
     return fetch('https://chat.homebots.io/chat', options as any).then((x) => x.json());
   }
