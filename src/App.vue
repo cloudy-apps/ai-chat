0c83722bd3e7400335bdac8b540b89f45f90aaf8
Darlan Alves <me@darlanalv.es>
2023-04-19 23:21:37 +0200

    fix: replace icon and avatar balloon

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="flex flex-col h-screen bg-gray-100">
-    <header class="bg-indigo-900 text-white text-2xl font-bold p-4">
-      Jarvis
+    <header class="bg-indigo-900 text-white text-2xl font-bold p-4 flex items-center"> 
+      <span class="w-full">Jarvis</span>
+      <div class="text-indigo-900 font-bold w-10 h-10 rounded-full flex items-center justify-center bg-gray-100 border border-gray-300 overflow-hidden">
+        <img src="/icon-72.png" alt="" />
+      </div>
     </header>
-    <main
-      class="flex flex-col flex-grow p-4 overflow-y-auto"
-    >
+    <main class="flex flex-col flex-grow p-4 overflow-y-auto">
       <MessageCard
         v-for="(message, index) in history"
         :key="index"
--- src/components/MessageCard.vue
+++ src/components/MessageCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="rounded-lg shadow-md px-4 py-2 text-gray-800 mb-4 relative"
+    class="rounded-lg shadow-md p-4 text-gray-800 mb-4 relative"
     :class="(isAssistant && 'bg-blue-100 ml-12') || 'bg-white'"
   >
     <div class="flex absolute top-0 right-0 mr-2 mt-1">
@@ -11,63 +11,50 @@
         <span class="material-icons text-gray-500 focus:text-gray-600">close</span>
       </button>
     </div>
-
-    <div class="flex items-top space-x-4">
-      <div class="shrink-0">
-        <div
-          class="text-indigo-900 font-bold w-12 h-12 rounded-full flex items-center justify-center bg-gray-100 border border-gray-300"
-        >
-          {{ isAssistant ? 'AI' : 'ME' }}
-        </div>
-      </div>
-      <div>
-        <h1 class="text-md font-bold mb-2">
-          {{ isAssistant ? 'Jarvis' : 'Me' }}
-        </h1>
-        <div v-if="!htmlMessage" class="text-base whitespace-pre-wrap">{{ message.content }}</div>
-        <div v-else v-html="htmlMessage" class="text-base whitespace-pre-wrap"></div>
-      </div>
-    </div>
+    <h1 class="font-bold mb-2">{{ isAssistant ? 'Jarvis' : 'Me' }}</h1>
+    <div v-if="!htmlMessage">{{ message.content }}</div>
+    <div v-else v-html="htmlMessage" class="whitespace-pre-wrap"></div>
   </div>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from "vue";
+import { defineComponent } from "vue";
 
-  export default defineComponent({
-    props: {
-      message: {
-        type: Object,
-        required: false,
-        default: () => ({ role: "", content: "" }),
-      },
-      id: {
-        type: Number,
-        default: -1,
-      },
+export default defineComponent({
+  props: {
+    message: {
+      type: Object,
+      required: false,
+      default: () => ({ role: "", content: "" }),
     },
-    data: () => ({ htmlMessage: '' }),
-    computed: {
-      isMe() {
-        return this.message.role === "user";
-      },
-      isAssistant() {
-        return this.message.role === "assistant";
-      },
+  },
+  data: () => ({ htmlMessage: '' }),
+  computed: {
+    isMe() {
+      return this.message.role === "user";
     },
-    methods: {
-      removeMessage() {
-        this.$emit("remove");
-      },
-      renderMessage() {
-        fetch('https://markdown.jsfn.run/', {
-          mode: 'cors',
-          method: 'post',
-          body: this.message.content
-        })
-          .then(x => x.text())
-          .then(html => this.htmlMessage = html);
+    isAssistant() {
+      return this.message.role === "assistant";
+    },
+  },
+  methods: {
+    removeMessage() {
+      this.$emit("remove");
+    },
+    renderMessage() {
+      if (this.htmlMessage) {
+        this.htmlMessage = '';
+        return;
       }
-    },
-  });
+
+      fetch('https://markdown.jsfn.run/', {
+        mode: 'cors',
+        method: 'post',
+        body: this.message.content
+      })
+        .then(x => x.text())
+        .then(html => this.htmlMessage = html);
+    }
+  },
+});
 </script>
