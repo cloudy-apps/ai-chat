b776e30754d23fa91c740c92db57a58a6c0cce15
Darlan Alves <me@darlanalv.es>
2023-08-08 09:52:13 +0200

    chore: upgrade CI builder

--- components/App.vue
+++ components/App.vue
@@ -22,12 +22,12 @@
 
     <div class="flex flex-col h-screen bg-gray-100">
       <header
-        class="bg-indigo-900 text-white text-2xl font-bold p-4 flex items-center"
+        class="bg-primary text-white text-2xl font-bold p-4 flex items-center"
       >
         <h1 class="w-full">{{ bot }}</h1>
         <button
           @click="selectBot = true"
-          class="text-indigo-900 font-bold w-10 h-10 rounded-full flex items-center justify-center bg-gray-100 border border-gray-300 overflow-hidden"
+          class="text-primary font-bold w-10 h-10 rounded-full flex items-center justify-center bg-gray-100 border border-gray-300 overflow-hidden"
         >
           <img src="/icon-72.png" alt="" />
         </button>
